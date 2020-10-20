150219a840d64fc9d725ca5591896493d5af51b9
Kasey Jones <joneskm72@gmail.com>
2020-10-20 16:09:24 -0400

    Updated structure

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
