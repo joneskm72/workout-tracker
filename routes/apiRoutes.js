50f1ef13674e3c0828c8bcbc11d10b3b3b3d1944
Kasey Jones <joneskm72@gmail.com>
2020-10-22 17:31:19 -0400

    Updated routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,15 +9,15 @@ const router = require("express").Router();
   });
 
 
-  router.post("/api/workout", function(req, res) {
+  router.post("/api/workouts", function(req, res) {
    Workout.create({}).then(dbWorkout => res.json(dbWorkout)).catch(err => res.json(err))
   });
 
-   router.put("/api/workout/:id", (req, res) => {
+   router.put("/api/workouts/:id", (req, res) => {
     Workout.findByIdAndUpdate(req.params.id,  {$push: {exercises: req.body}})
    })
 
-  router.delete("/api/workout/:id", function(req, res) {
+  router.delete("/api/workouts/:id", function(req, res) {
     Workout.findByIdAndDelete(req.body.id).then(() => res.json({ ok: true })).catch(err => res.status(500).json(err))
   });
 
