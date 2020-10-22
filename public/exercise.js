9672d6f5c6488c3a199215e86360cd56e284b169
Kasey Jones <joneskm72@gmail.com>
2020-10-22 15:14:09 -0400

    testing location

--- public/exercise.js
+++ public/exercise.js
@@ -19,7 +19,7 @@ let shouldNavigateAway = false;
 
 async function initExercise() {
   let workout;
-  console.log(location.search);
+  console.log(location);
   if (location.search.split("=")[1] === undefined) {
     workout = await API.createWorkout()
     console.log(workout)
